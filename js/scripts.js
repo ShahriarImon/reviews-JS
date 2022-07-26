70bb6cc55af87282fcc127f6bffd8f48fdfc0fc5
ShahriarImon <imon.shahriar012@gmail.com>
2022-07-26 15:28:05 +0600

    1st comment

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const reset = document.getElementById("reset");
+const counter = document.getElementById("counter");
